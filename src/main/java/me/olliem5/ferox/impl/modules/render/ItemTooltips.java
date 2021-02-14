28dd1a066569649cad3a69d9f23fb22498acf1ba
ollie <64110522+olliem5@users.noreply.github.com>
2021-02-14 17:32:17 +1100

    finished shulker tooltip

--- src/main/java/me/olliem5/ferox/impl/modules/render/ItemTooltips.java
+++ src/main/java/me/olliem5/ferox/impl/modules/render/ItemTooltips.java
@@ -5,6 +5,8 @@ import me.olliem5.ferox.api.module.FeroxModule;
 import me.olliem5.ferox.api.module.Module;
 import me.olliem5.ferox.api.util.render.font.FontUtil;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.item.ItemStack;
 
 import java.awt.*;
@@ -16,10 +18,19 @@ import java.awt.*;
 @FeroxModule(name = "ItemTooltips", description = "Shows handy tooltips for shulker boxes and maps", category = Category.RENDER)
 public final class ItemTooltips extends Module {
     public static void renderShulkerTooltip(ItemStack itemStack, int x, int y) {
-        Gui.drawRect(x + 8, y - 21, x + 158, y - 8, new Color(100, 20, 20, 200).getRGB());
-        Gui.drawRect(x + 8, y - 6, x + 158, y + 49, new Color(0, 0, 0, 150).getRGB());
+        GlStateManager.disableLighting();
+        GlStateManager.disableDepth();
+
+        Gui.drawRect(x + 8, y - 21, x + 158, y - 6, new Color(100, 20, 20, 225).getRGB());
+        Gui.drawRect(x + 8, y - 6, x + 158, y + 48, new Color(0, 0, 0, 225).getRGB());
 
         FontUtil.drawString(itemStack.getDisplayName(), x + 10, y - 18, -1);
+
+        GlStateManager.enableDepth();
+
+        mc.getRenderItem().zLevel = 150.0f;
+
+        RenderHelper.enableGUIStandardItemLighting();
 
         for (int i = 0; i < 27; i++) {
             int offsetX = x + (i % 9) * 16 + 11;
@@ -28,5 +39,11 @@ public final class ItemTooltips extends Module {
             mc.getRenderItem().renderItemAndEffectIntoGUI(itemStack, offsetX, offsetY);
             mc.getRenderItem().renderItemOverlayIntoGUI(mc.fontRenderer, itemStack, offsetX, offsetY, null);
         }
+
+        RenderHelper.disableStandardItemLighting();
+
+        mc.getRenderItem().zLevel = 0.0f;
+
+        GlStateManager.enableLighting();
     }
 }
