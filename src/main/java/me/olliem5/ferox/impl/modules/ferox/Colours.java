c82fecdf183a5c9d13dada139abad8640c6c88b0
ollie <64110522+olliem5@users.noreply.github.com>
2021-02-15 22:32:54 +1100

    oh no!! huge bugfix...

--- src/main/java/me/olliem5/ferox/impl/modules/ferox/Colours.java
+++ src/main/java/me/olliem5/ferox/impl/modules/ferox/Colours.java
@@ -8,7 +8,7 @@ import me.olliem5.ferox.api.setting.Setting;
 import java.awt.*;
 
 @FeroxModule(name = "Colours", description = "Controls global client colours", category = Category.Ferox)
-public class Colours extends Module {
+public final class Colours extends Module {
     public static final Setting<Boolean> clientColour = new Setting<>("Client Colour", "The global colour for Ferox", true);
     public static final Setting<Color> clientColourPicker = new Setting<>(clientColour, "Client Colour Picker", "The global colour for Ferox", new Color(98, 35, 188, 255));
 
