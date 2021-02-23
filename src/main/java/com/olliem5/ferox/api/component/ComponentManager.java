104f966d6b48cb6afcc7cd9834e0dcd505af9e63
HowWu-Not-a-chink <70532444+HowWu-Not-a-chink@users.noreply.github.com>
2021-02-23 00:15:20 +0000

    Coords, with 2 options. Might make mode ;)

--- src/main/java/com/olliem5/ferox/api/component/ComponentManager.java
+++ src/main/java/com/olliem5/ferox/api/component/ComponentManager.java
@@ -20,6 +20,7 @@ public final class ComponentManager implements Minecraft {
     public static void init() {
         components.addAll(Arrays.asList(
                 new ArraylistComponent(),
+                new CoordsComponent(),
                 new InventoryComponent(),
                 new NotificationComponent(),
                 new PlayerComponent(),
