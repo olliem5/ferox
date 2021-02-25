7f60725ea498340880ea264cba2f80fdba1feed3
HowWu-Not-a-chink <70532444+HowWu-Not-a-chink@users.noreply.github.com>
2021-02-25 23:58:13 +0000

    added getfacing to rotation util for direction component

--- src/main/java/com/olliem5/ferox/api/util/packet/RotationUtil.java
+++ src/main/java/com/olliem5/ferox/api/util/packet/RotationUtil.java
@@ -1,7 +1,9 @@
 package com.olliem5.ferox.api.util.packet;
 
+import com.mojang.realmsclient.gui.ChatFormatting;
 import com.olliem5.ferox.api.traits.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.MathHelper;
 
 /**
  * @author linustouchtips
@@ -72,5 +74,28 @@ public final class RotationUtil implements Minecraft {
         yaw += 90.0;
 
         return new double[]{yaw, pitch};
+    }
+
+
+    public static String getFacing() {
+        switch (MathHelper.floor(mc.player.rotationYaw + 8.0f / 360.0f + 0.5) & 0x7) {
+            case 0:
+                return " [" + ChatFormatting.WHITE + "Z+" + ChatFormatting.RESET + "]";
+            case 1:
+                return " [" + ChatFormatting.WHITE + "Z+" + ChatFormatting.RESET + "]";
+            case 2:
+                return " [" + ChatFormatting.WHITE + "X-" + ChatFormatting.RESET + "]";
+            case 3:
+                return " [" + ChatFormatting.WHITE + "X-" + ChatFormatting.RESET + "]";
+            case 4:
+                return " [" + ChatFormatting.WHITE + "Z-" + ChatFormatting.RESET + "]";
+            case 5:
+                return " [" + ChatFormatting.WHITE + "Z-" + ChatFormatting.RESET + "]";
+            case 6:
+                return " [" + ChatFormatting.WHITE + "X+" + ChatFormatting.RESET + "]";
+            case 7:
+                return " [" + ChatFormatting.WHITE + "X+" + ChatFormatting.RESET + "]";
+        }
+        return "Invalid";
     }
 }
